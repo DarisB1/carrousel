e1fb31a9f99392830424a780c5379bf6ec077652
DarisB1 <dboumertit@edenschool.fr>
2024-05-27 17:16:41 +0200

    carrousel btn droit

---
+++ main.js
@@ -0,0 +1,49 @@
+const body = document.querySelector('body')
+let droite = document.querySelector('#d')
+
+// ajouter un argument qui vaut l'index de db
+const db = [
+  {
+      "name":"chat",
+      "url":"chat.webp",
+      "alt":"chat brun",
+      "date":"02/08/23"
+  },
+  {
+      "name":"chien",
+      "url":"chien.jpg",
+      "alt":"chien brun",
+      "date":"20/04/23"
+  },
+  {
+      "name":"gecko",
+      "url":"leopard.jpg",
+      "alt":"gecko tacheté léopard",
+      "date":"12/12/12"
+  }
+]
+
+let div = document.createElement('div');
+
+
+
+function changeImg (i) {
+  div.innerHTML= `
+  <h1> ${db[i].name} </h1>
+  <img src="${db[i].url}" alt="${db[i].alt}" />
+  <p>${db[i].date}</p>
+  `
+}
+
+let counter = 0;
+changeImg(counter)
+body.appendChild(div)
+
+droite.addEventListener("click", ()=> {
+counter++
+if(counter === db.length) {
+  counter = 0;
+}
+
+  changeImg(counter)
+});
