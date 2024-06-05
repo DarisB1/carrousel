ddefd4cf0cd8ea5f5be24ece580b82e98bd449a7
DarisB1 <dboumertit@edenschool.fr>
2024-06-05 16:05:45 +0200

    v1

--- main.js
+++ main.js
@@ -1,37 +1,36 @@
-const body = document.querySelector('body')
-let droite = document.querySelector('#d')
-let gauche = document.querySelector('#g')
+const body = document.querySelector("body");
+let droite = document.querySelector("#d");
+let gauche = document.querySelector("#g");
+let fin;
 
-// ajouter un argument qui vaut l'index de db
 const db = [
   {
-      "name":"chat",
-      "url":"chat.webp",
-      "alt":"chat brun",
-      "date":"02/08/23"
+    name: "chat",
+    url: "chat.webp",
+    alt: "chat brun",
+    date: "02/08/23",
   },
   {
-      "name":"chien",
-      "url":"chien.jpg",
-      "alt":"chien brun",
-      "date":"20/04/23"
+    name: "chien",
+    url: "chien.jpg",
+    alt: "chien brun",
+    date: "20/04/23",
   },
   {
-      "name":"gecko",
-      "url":"leopard.jpg",
-      "alt":"gecko tacheté léopard",
-      "date":"12/12/12"
-  }
-]
+    name: "gecko",
+    url: "leopard.jpg",
+    alt: "gecko tacheté léopard",
+    date: "12/12/12",
+  },
 
-let div = document.createElement('div');
+];
 
+const div = document.createElement("div");
 
-
-function changeImg (i) {
-  div.innerHTML= `
-  <h1> ${db[i].name} </h1>
-  <img src="${db[i].url}" alt="${db[i].alt}" />
+function changeImg(i) {
+  div.innerHTML = `
+  <h1>${db[i].name}</h1>
+  <img src="${db[i].url}" class="tous" alt="${db[i].alt}">
   <p>${db[i].date}</p>
   `
 }
@@ -40,20 +39,37 @@ let counter = 0;
 changeImg(counter)
 body.appendChild(div)
 
-droite.addEventListener("click", ()=> {
-counter++
-if(counter === db.length) {
-  counter = 0;
-}
-
+droite.addEventListener("click", ()=>{
+  clearInterval(fin);
+  counter++
+  if(counter === db.length){
+    counter = 0
+  }
+  fin = setInterval(() => {
+    counter++
+  if(counter === db.length){
+    counter = 0
+  }
+  changeImg(counter)
+  }, 5000);
   changeImg(counter)
 });
 
-gauche.addEventListener("click", ()=> {
+
+gauche.addEventListener("click", ()=>{
   counter--
-  if(counter < 0) {
-    counter = db.length - 1;
+  if(counter < 0){
+    counter = db.length - 1
   }
-  
-    changeImg(counter)
-  });
+  changeImg(counter)
+});
+
+
+fin = setInterval(() => {
+  counter++
+  if(counter === db.length){
+    counter = 0
+  }
+  changeImg(counter)
+}, 1000);
+
